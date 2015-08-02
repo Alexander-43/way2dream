4aa15e86cdb6f0bc589db4854706101744967de8
Alexander <kerhac@rambler.ru>
2015-08-02 22:02:31 +0300

    pdf print form

--- sql/game/inc/get_pdf.php
+++ sql/game/inc/get_pdf.php
@@ -1,8 +1,11 @@
 <?php
 
+include_once 'tcpdf/config/tcpdf_config.php';
+include_once 'tcpdf/tcpdf.php';
 include_once 'objectExtractor/includes.inc';
 include_once 'PdfGenerator.inc';
 
 $pdf = new PDF(@file_get_contents("http://sqltest.ru/editUserData.php?id=eac929db221ad7ab4801f04c18cfecb7"), null, array('input'));
-print_r($pdf);
+$pdf->getPdf();
+
 ?>
