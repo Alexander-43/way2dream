89b8c5822c9593dcc90aabf431b2d95b247c83c0
Alexander <kerhac@rambler.ru>
2015-08-17 00:28:05 +0300

    format

--- sql/game/inc/get_pdf.php
+++ sql/game/inc/get_pdf.php
@@ -12,9 +12,13 @@ $skip = array('/winnerPlan\d*/',
 		'/h_state_\d*/', 
 		'/h_happend_\d*/',
 		'/h_time_\d*/',
-		'/sourceIncome\S*/'
-);
-$pdf = new PDF(@file_get_contents("http://sqltest.ru/editUserData.php?id=eac929db221ad7ab4801f04c18cfecb7"), null, array('style', 'input', 'div[id=table1]', 'div[id=text1]', 'div[id=text2]', 'div[id=text3]', 'div[id=text4]', 'div[id=table2]'), $skip);
+		'/sourceIncome\S*/');
+
+$pdf = new PDF(@file_get_contents("http://sqltest.ru/editUserData.php?id=eac929db221ad7ab4801f04c18cfecb7"), 
+				null, 
+				array('style', 'input','div[id=table1]','div[id=text1]','div[id=text2]','div[id=text3]','div[id=text4]','div[id=table2]'), 
+				$skip);
+
 $pdf->getPdf();
 
 ?>
