1ecab4c8193d277b9336831770f4e16a33ec3b1f
Alexander <kerhac@rambler.ru>
2015-08-19 22:30:04 +0300

    add content processor

--- sql/game/inc/get_pdf.php
+++ sql/game/inc/get_pdf.php
@@ -13,8 +13,8 @@ $skip = array('/winnerPlan\d*/',
 		'/h_happend_\d*/',
 		'/h_time_\d*/',
 		'/sourceIncome\S*/');
-
-$pdf = new PDF(@file_get_contents("http://sqltest.ru/editUserData.php?id=eac929db221ad7ab4801f04c18cfecb7"), 
+$contentGetter = new contentReturnerProcessor();
+$pdf = new PDF($contentGetter->get_content("http://sqltest.ru/editUserData.php?id=eac929db221ad7ab4801f04c18cfecb7"), 
 				null, 
 				array('style', 'input','div[id=table1]','div[id=text1]','div[id=text2]','div[id=text3]','div[id=text4]','div[id=table2]'), 
 				$skip);
